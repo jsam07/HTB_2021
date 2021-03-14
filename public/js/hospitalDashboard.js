6d255360aa93e58ded9ecdca54d0e6b25d2fd3f1
matteomiceli <omatteomiceli@gmail.com>
2021-03-13 22:28:25 -0800

    cards as links, no more dom!

--- public/js/hospitalDashboard.js
+++ public/js/hospitalDashboard.js
@@ -1,16 +1,3 @@
-$(document).ready(() => {
-    const patientId = $('p.title');
-   
-    // Click function to send data
-    patientId.click((event) => {
-        console.log($(this).attr('inner html'))
-    })
-
-
-    // Click?
-    $(document).on("click",".appDetails", function () {
-        var clickedBtnID = $(this).attr('class'); // or var clickedBtnID = this.id
-        alert('you clicked on button #' + clickedBtnID);
-     });
-    
+$(() => {
+    // no more need to pull from dom, can just make a button
 });
