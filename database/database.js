de8f472336902730a90dd17cfa25fcd933714863
clam169 <jojilam@gmail.com>
2021-03-14 00:36:30 -0800

    small changes

---
+++ database/database.js
@@ -0,0 +1,75 @@
+const patients = {
+    123456: {
+        symptoms: [
+            'shortness of breath', 'tightness in chest', 'dizziness', 'nausea'
+        ],
+        triage: 'green',
+        firstName: 'Jane',
+        lastName: 'Doe',
+        age: 44,
+        sex: 'F',
+        chronicIllnesses: ["Diabetes", "Hypertension"],
+        medications: ["Canagliflozin", "Lisinopril"],
+        isSmoker: true,
+        isPregnant: false,
+        vitals: [
+            {
+                timestamp: "12:32", // new Date().toString()
+                hr: 54, // number
+                rr: 24, // number
+                bp: {
+                    systolic: 140,
+                    diastolic: 90,
+                    }, // bp.systolic + "/" bp.distolic
+                temp: 38, // number
+                spO2: 89,
+                notes: {
+                    pain: "",
+                    LOC: "",
+                    "N/V": "",
+                },
+            }
+        ],
+        allergies: ["peanuts", "anesthetics", "cats", "gluten"],
+        reportedSymptoms: ["breathlessness", "chest pain"],
+        other: "this lady full of regret",
+    },
+
+    543232: {
+        symptoms: [
+            'shortness of breath', 'tightness in chest', 'dizziness', 'nausea'
+        ],
+        triage: 'green',
+        firstName: 'Jane',
+        lastName: 'Doe',
+        age: 44,
+        sex: 'F',
+        chronicIllnesses: ["Diabetes", "Hypertension"],
+        medications: ["Canagliflozin", "Lisinopril"],
+        isSmoker: true,
+        isPregnant: false,
+        vitals: [
+            {
+                timestamp: "12:32", // new Date().toString()
+                hr: 54, // number
+                rr: 24, // number
+                bp: {
+                    systolic: 140,
+                    diastolic: 90,
+                    }, // bp.systolic + "/" bp.distolic
+                temp: 38, // number
+                spO2: 89,
+                notes: {
+                    pain: "",
+                    LOC: "",
+                    "N/V": "",
+                },
+            }
+        ],
+        allergies: ["peanuts", "anesthetics", "cats", "gluten"],
+        reportedSymptoms: ["breathlessness", "chest pain"],
+        other: "this lady full of regret",
+    },
+};
+
+module.exports = patients;
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const database = require('../database/database.js')
 
 const router = express.Router();
 
