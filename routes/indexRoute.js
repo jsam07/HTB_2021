7bc3886cd435c8c3bca717b1199c49c00ea4cb04
matteomiceli <omatteomiceli@gmail.com>
2021-03-13 16:40:30 -0800

    starting logic on hospital-dash ejs

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('hospital-admin'); // change back to index.ejs 
 });
 
 module.exports = router;
