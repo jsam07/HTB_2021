27a07eba38bc63add4724a0270b0ee22de767dfd
jfoxx77 <jsam07@hotmail.com>
2021-03-14 01:11:51 -0800

    Fixed Routing

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const database = require('../database/database.js')
+const database = require('../database/database.js');
 
 const router = express.Router();
 
 router.get('/patient-dashboard', (req, res) => {
-    // Get patient ID first before rendering
-    console.log('Got here');
     res.render('patient-dashboard');
 });
 
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,26 +1,25 @@
-const express = require('express')
+const express = require('express');
 // const { ensureAuthenticated } = require('../middleware/checkAuth');
-const database = require('../database/database.js')
+const database = require('../database/database.js');
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index')
-})
+    res.render('index');
+});
 
-//---------- EMT Route -----------/ /
+// ---------- EMT Route -----------/ /
 // localhost:8081/emt/dashboard
 router.get('/emt-dashboard', (req, res) => {
-    //send correct patient to emt
-    let userID = 
-    res.render('emt-dashboard', { patient: patients[0], vitals: allVitals })
-})
+    // send correct patient to emt
+    const userID = res.render('emt-dashboard', { patient: patients[0], vitals: allVitals });
+});
 
 router.get('/hospital-dashboard', (req, res) => {
-    res.render('hospital-admin', { patients: database }) // change back to index.ejs
-})
+    res.render('hospital-admin', { patients: database }); // change back to index.ejs
+});
 
-module.exports = router
+module.exports = router;
 
 // hardcoded patient data
 
