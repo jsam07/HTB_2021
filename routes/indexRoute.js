1d09672ce3fd977a7696cd916574dfec4fb47bec
matteomiceli <omatteomiceli@gmail.com>
2021-03-13 17:10:37 -0800

    hardcode logic working, basic data structure

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -4,7 +4,20 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('hospital-admin'); // change back to index.ejs 
+    res.render('hospital-admin', { patients: patients }); // change back to index.ejs 
 });
 
 module.exports = router;
+
+
+
+
+// hardcoded patient data
+
+let patients = [
+    {
+    id: Math.floor(100000 + Math.random() * 900000),
+    symptoms: ['shortness of breath, tightness in chest, dizziness, nausia'],
+    triage: 'green'
+}
+];
